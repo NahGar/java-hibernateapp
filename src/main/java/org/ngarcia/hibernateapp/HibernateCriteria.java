1ca29773fbeb9bb709b38ad3b53d193c93dc43cf
Nahuel Garcia <nhlgrc@hotmail.com>
2025-03-20 15:09:28 -0300

    desde saico

--- src/main/java/org/ngarcia/hibernateapp/HibernateCriteria.java
+++ src/main/java/org/ngarcia/hibernateapp/HibernateCriteria.java
@@ -5,6 +5,8 @@ import jakarta.persistence.criteria.*;
 import org.ngarcia.hibernateapp.entity.Cliente;
 import org.ngarcia.hibernateapp.util.JpaUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HibernateCriteria {
@@ -53,6 +55,52 @@ public class HibernateCriteria {
       query.select(from).where(criteria.between(from.get("id"),2L,4L));
       clientes = em.createQuery(query).getResultList();
       clientes.forEach(System.out::println);
+
+      System.out.println("---- usando where in ----");
+      query = criteria.createQuery(Cliente.class);
+      from = query.from(Cliente.class);
+      //query.select(from).where(from.get("nombre").in("Gael","Micaela"));
+      //clientes = em.createQuery(query).getResultList();
+      ParameterExpression<List> listParam = criteria.parameter(List.class,"nombres");
+      query.select(from).where(from.get("nombre").in(listParam));
+      clientes = em.createQuery(query).setParameter("nombres", Arrays.asList("Gael","Micaela")).getResultList();
+      clientes.forEach(System.out::println);
+
+      System.out.println("---- usando where > >= ----");
+      query = criteria.createQuery(Cliente.class);
+      from = query.from(Cliente.class);
+      //query.select(from).where(criteria.greaterThan(from.get("id"), 3L));
+      query.select(from).where(criteria.greaterThanOrEqualTo(from.get("id"), 3L));
+      clientes = em.createQuery(query).getResultList();
+      clientes.forEach(System.out::println);
+
+      System.out.println("---- usando where < <= ----");
+      query = criteria.createQuery(Cliente.class);
+      from = query.from(Cliente.class);
+      //query.select(from).where(criteria.lessThan(from.get("id"), 3L));
+      query.select(from).where(criteria.lessThanOrEqualTo(from.get("id"), 3L));
+      clientes = em.createQuery(query).getResultList();
+      clientes.forEach(System.out::println);
+
+      System.out.println("---- usando and y or ----");
+      query = criteria.createQuery(Cliente.class);
+      from = query.from(Cliente.class);
+      Predicate porNombre = criteria.equal(from.get("nombre"),"Gael");
+      Predicate porFormaPago = criteria.equal(from.get("formaPago"),"debito");
+      Predicate porId = criteria.greaterThanOrEqualTo(from.get("id"),4L);
+      //query.select(from).where(criteria.and(porNombre, porFormaPago)); //AND
+      //query.select(from).where(criteria.or(porNombre, porFormaPago)); //OR
+      query.select(from).where(criteria.and(porId,criteria.or(porNombre, porFormaPago))); //AMBOS
+      clientes = em.createQuery(query).getResultList();
+      clientes.forEach(System.out::println);
+
+      System.out.println("---- consulta con order by ----");
+      query = criteria.createQuery(Cliente.class);
+      from = query.from(Cliente.class);
+      query.select(from).orderBy(criteria.desc(from.get("formaPago")),criteria.asc(from.get("nombre")));
+      clientes = em.createQuery(query).getResultList();
+      clientes.forEach(System.out::println);
+
 
       em.close();
    }
